d41c0768c6f31170a3cff1ad0a4d075995758400
Matt Boardman <askboardman@gmail.com>
2018-04-29 00:39:30 -0500

    Added comments for Tree and Node structs

--- search.go
+++ search.go
@@ -8,12 +8,17 @@ import (
 	uuid "github.com/google/uuid"
 )
 
+// Node struct contains:
+// 1. Reference to a ToDo item
+// 2. Reference to Left Node
+// 3. Reference to Right Node
 type Node struct {
 	ToDo  *ToDo
 	Left  *Node
 	Right *Node
 }
 
+// InsertByID inserts a ToDo item by its UUID
 func (n *Node) InsertByID(todo *ToDo) error {
 	if n == nil {
 		return errors.New("Cannot INSERT: Tree has not been instantiated")
@@ -38,6 +43,9 @@ func (n *Node) InsertByID(todo *ToDo) error {
 	return nil
 }
 
+// InsertByString inserts a ToDo item by a string value
+// If the Title exists then the Title is inserted as another
+// tree by ID
 func (n *Node) InsertByString(todo *ToDo) error {
 	if n == nil {
 		return errors.New("Cannot INSERT: Tree has not been instantiated")
@@ -65,6 +73,8 @@ func (n *Node) InsertByString(todo *ToDo) error {
 	return nil
 }
 
+// FindByID method traverses the Tree recursively
+// until it finds the matching UUID
 func (n *Node) FindByID(id uuid.UUID) *ToDo {
 	if n == nil {
 		return nil
@@ -80,6 +90,8 @@ func (n *Node) FindByID(id uuid.UUID) *ToDo {
 	}
 }
 
+// FindByString method traverses the Tree recursively
+// until it finds the matching string value
 func (n *Node) FindByString(value string) *ToDo {
 	if n == nil {
 		return nil
@@ -96,6 +108,7 @@ func (n *Node) FindByString(value string) *ToDo {
 	}
 }
 
+// findMax finds the right most element of the Tree
 func (n *Node) findMax(parent *Node) (*Node, *Node) {
 	if n == nil {
 		return nil, parent
@@ -106,6 +119,7 @@ func (n *Node) findMax(parent *Node) (*Node, *Node) {
 	return n.Right.findMax(n)
 }
 
+// replaceNode replaces the node with its right most value in the subtree
 func (n *Node) replaceNode(parent, replacement *Node) error {
 	if n == nil {
 		return errors.New("Cannot REPLACE: node to replace is nil")
@@ -119,6 +133,7 @@ func (n *Node) replaceNode(parent, replacement *Node) error {
 	return nil
 }
 
+// DeleteByID deletes a Node by its UUID
 func (n *Node) DeleteByID(id uuid.UUID, parent *Node) error {
 	if n == nil {
 		return errors.New("Cannot DELETE: ID does not exist in the tree")
@@ -148,6 +163,7 @@ func (n *Node) DeleteByID(id uuid.UUID, parent *Node) error {
 	}
 }
 
+// DeleteByString deletes a Node by a string value
 func (n *Node) DeleteByString(value string, parent *Node) error {
 	if n == nil {
 		return errors.New("Cannot DELETE: ID does not exist in the tree")
@@ -177,10 +193,12 @@ func (n *Node) DeleteByString(value string, parent *Node) error {
 	}
 }
 
+// Tree struct contains the root node
 type Tree struct {
 	Root *Node
 }
 
+// InsertByID inserts a ToDo item by its UUID
 func (t *Tree) InsertByID(todo *ToDo) error {
 	if t.Root == nil {
 		t.Root = &Node{ToDo: todo}
@@ -189,6 +207,7 @@ func (t *Tree) InsertByID(todo *ToDo) error {
 	return t.Root.InsertByID(todo)
 }
 
+// InsertByString inserts a ToDo item by its string value
 func (t *Tree) InsertByString(todo *ToDo) error {
 	if t.Root == nil {
 		t.Root = &Node{ToDo: todo}
@@ -197,6 +216,7 @@ func (t *Tree) InsertByString(todo *ToDo) error {
 	return t.Root.InsertByString(todo)
 }
 
+// FindByID finds a Node by its UUID
 func (t *Tree) FindByID(id uuid.UUID) *ToDo {
 	if t.Root == nil {
 		return nil
@@ -204,6 +224,7 @@ func (t *Tree) FindByID(id uuid.UUID) *ToDo {
 	return t.Root.FindByID(id)
 }
 
+// FindByString finds a Node by its string value
 func (t *Tree) FindByString(value string) *ToDo {
 	if t.Root == nil {
 		return nil
@@ -211,6 +232,7 @@ func (t *Tree) FindByString(value string) *ToDo {
 	return t.Root.FindByString(value)
 }
 
+// DeleteByID removes a Node by its UUID
 func (t *Tree) DeleteByID(id uuid.UUID) error {
 	if t.Root == nil {
 		return errors.New("Cannot DELETE: tree is empty")
@@ -227,6 +249,7 @@ func (t *Tree) DeleteByID(id uuid.UUID) error {
 	return nil
 }
 
+// DeleteByString removes a Node by its string value
 func (t *Tree) DeleteByString(value string) error {
 	if t.Root == nil {
 		return errors.New("Cannot DELETE: tree is empty")
